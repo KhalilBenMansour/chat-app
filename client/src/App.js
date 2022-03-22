e730e2a8728f78c5c0bebf73df4270093c3efc49
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-03-22 23:33:51 +0100

    Initiate app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChatBox from "./components/chatBox/ChatBox";
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/chatBox/ChatBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./chatBox.scss";
+const ChatBox = () => {
+  return <div className="chatBox">ChatBox</div>;
+};
+
+export default ChatBox;
