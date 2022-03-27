3cc909ac5dfbf71683d2f9a138b872a8bef1a53b
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-03-27 14:20:29 +0100

    style chatContent

--- client/src/components/chatContent/ChatContent.js
+++ client/src/components/chatContent/ChatContent.js
@@ -1,6 +1,25 @@
 import "./chatContent.scss";
 const ChatContent = () => {
-  return <div className="chatContent">chatContent</div>;
+  return (
+    <div className="chatContent">
+      <div className="msgs">
+        <div className="my-msg">
+          hi oussema thanks for your support you are such a generous man who
+          helped me when i were stuck
+        </div>
+        <div className="my-msg">hi ous</div>
+
+        <div className="other-msg">thank you</div>
+        <div className="other-msg">hi khalil</div>
+
+        <div className="my-msg">
+          hi oussema thanks for your support you are such a generous man who
+          helped me when i were stuck
+        </div>
+        <div className="my-msg">hi ous</div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatContent;
