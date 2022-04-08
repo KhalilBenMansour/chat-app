fa5c781d84d8b0a7bdce5ebad9dfe6effd9a4404
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-09 00:59:18 +0100

    Add fake data

--- client/src/dummy.js
+++ client/src/dummy.js
@@ -6,17 +6,20 @@ export const msgs = [
   {
     msgId: 1,
     text: "hi oussema",
+    photo: "khalil.jpg",
     userId: 1,
     currentUser: true,
   },
   {
     msgId: 2,
     text: "hi oussema thanks for your support you are such a generous man whhelped me when i were stuck",
+    photo: "khalil.jpg",
     userId: 1,
     currentUser: true,
   },
   {
     msgId: 3,
+    photo: "khalil.jpg",
     text: "hi khalil thanks",
     userId: 2,
     currentUser: false,
@@ -24,18 +27,21 @@ export const msgs = [
   {
     msgId: 4,
     text: "that's all",
+    photo: "khalil.jpg",
     userId: 1,
     currentUser: true,
   },
   {
     msgId: 5,
     text: "yes",
+    photo: "khalil.jpg",
     userId: 2,
     currentUser: false,
   },
   {
     msgId: 6,
     text: "you are amazing take your time and learn correctly because its all about consistancy",
+    photo: "khalil.jpg",
     userId: 2,
     currentUser: false,
   },
