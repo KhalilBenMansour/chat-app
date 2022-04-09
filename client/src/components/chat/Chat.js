3b26fdee16a91472988059dff5dc67503db52225
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-09 01:00:13 +0100

    Add Chat.js

---
+++ client/src/components/chat/Chat.js
@@ -0,0 +1,13 @@
+import ChatBox from "../chatBox/ChatBox";
+import "./chat.scss";
+const Chat = () => {
+  return (
+    <div className="chat">
+      {/* <div>start</div> */}
+      <ChatBox />
+      {/* <div>end</div> */}
+    </div>
+  );
+};
+
+export default Chat;
