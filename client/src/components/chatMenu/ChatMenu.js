0135e1f831462b39211446d24b42aefdb7dbfbdf
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-10 03:26:38 +0100

    Update chatMenu component

--- client/src/components/chatMenu/ChatMenu.js
+++ client/src/components/chatMenu/ChatMenu.js
@@ -1,33 +1,10 @@
 import "./chatMenu.scss";
-import { AiOutlineUsergroupAdd } from "react-icons/ai";
-import { CgAddR } from "react-icons/cg";
-import ChatList from "../chatList/ChatList";
+
+import ChatFriends from "../chatFriends/ChatFriends";
 const ChatMenu = () => {
   return (
     <div className="chatMenu">
-      <header className="chat-header">
-        <div className="chat-left">
-          <h3 className="chat-title">Chats</h3>
-        </div>
-        <div className="chat-right">
-          <div className="chatMenu-button">
-            <AiOutlineUsergroupAdd />
-          </div>
-          <div className="chatMenu-button">
-            <CgAddR />
-          </div>
-        </div>
-      </header>
-      <section className="chat-body">
-        <form className="search-form">
-          <input
-            type="text"
-            placeholder="search chats ..."
-            className="search-input"
-          />
-        </form>
-        <ChatList />
-      </section>
+      <ChatFriends type="friends" />
     </div>
   );
 };
