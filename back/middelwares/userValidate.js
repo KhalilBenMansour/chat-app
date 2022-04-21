0732b14b70656afa57db26014de985476f517b93
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-22 00:58:11 +0100

    Add middelwares authentication and validation

---
+++ back/middelwares/userValidate.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+exports.userValidate = (req, res, next) => {
+  const schema = Joi.object({
+    userName: Joi.string().min(3).max(44).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  next();
+};
