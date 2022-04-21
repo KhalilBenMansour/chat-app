637678f361562492ad9e353f462543b399303324
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-21 03:18:13 +0100

    Add current user route and add middelwares

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const userController = require("../controllers/user");
+const { signUp, logIn } = require("../controllers/user");
+const { userValidate } = require("../middelwares/userValidate");
+const { auth } = require("../middelwares/auth");
 const router = express.Router();
 
-router.post("/signup", userController.signUp);
-router.post("/login", userController.logIn);
+router.post("/signup", userValidate, signUp);
+router.post("/login", logIn);
+router.get("/currentuser", auth, (req, res) => {
+  res.status(200).send(req.user);
+});
 
 module.exports = router;
