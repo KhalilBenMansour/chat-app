14db5d1628eaac21b2068c311e6198fad545a484
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-24 01:42:17 +0100

    Add and test verification Route

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signUp, logIn } = require("../controllers/user");
+const { signUp, logIn, verifyUser } = require("../controllers/user");
 const { userValidate } = require("../middelwares/userValidate");
 const { auth } = require("../middelwares/auth");
 const router = express.Router();
 
 router.post("/signup", userValidate, signUp);
+router.get("/verify/:id/:confirmationCode", verifyUser);
 router.post("/login", logIn);
 router.get("/currentuser", auth, (req, res) => {
   res.status(200).send(req.user);
