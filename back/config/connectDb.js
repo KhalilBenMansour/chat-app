fb5b14e2e327a103a3514e1de4361ac51a30df84
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-04-24 01:40:21 +0100

    Update database connection uri

--- back/config/connectDb.js
+++ back/config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
   mongoose.connect(
-    process.env.mongoAtlasUri,
+    process.env.mongoAtlasUri || "mongodb://localhost:27017/chatdb",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
