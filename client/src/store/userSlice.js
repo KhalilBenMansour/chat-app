d54b2799801bab18edbbcaf237115e8c9cde1d19
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-05-06 10:44:49 +0100

    Change fetch with axios

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -9,19 +9,20 @@ export const loginUser = createAsyncThunk(
     const { rejectWithValue } = thunkAPI;
 
     try {
-      // const res = await axios.post(`${BASE_URL}/login`, userCred);
+      const res = await axios.post(`${BASE_URL}/login`, userCred);
 
-      const res = await fetch(`${BASE_URL}/login`, {
-        method: "POST",
-        body: JSON.stringify(userCred),
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      const data = await res.json();
+      // const res = await fetch(`${BASE_URL}/login`, {
+      //   method: "POST",
+      //   body: JSON.stringify(userCred),
+      //   headers: {
+      //     "Content-type": "application/json",
+      //   },
+      // });
+      const data = res.data;
       return data;
     } catch (error) {
-      return rejectWithValue(error);
+      console.log(error);
+      return rejectWithValue(error.response.data);
     }
   }
 );
@@ -31,24 +32,36 @@ const userSlice = createSlice({
   initialState: {
     loading: false,
     loginSuccess: false,
+    token: null,
+    isAuth: false,
     error: null,
     message: null,
   },
   reducers: {},
   extraReducers: {
+    // loginUser
     [loginUser.pending]: (state, action) => {
       state.loading = true;
       state.error = null;
     },
     [loginUser.fulfilled]: (state, action) => {
+      console.log(action);
+      state.loading = false;
+      state.loginSuccess = action.payload.success;
+      state.isAuth = action.payload.success;
+      state.message = action.payload.msg;
+      state.token = action.payload.token;
+
+      localStorage.setItem("token", action.payload.token);
+    },
+    [loginUser.rejected]: (state, action) => {
+      console.log(action);
       state.loading = false;
       state.loginSuccess = action.payload.success;
       state.message = action.payload.msg;
-    },
-    [loginUser.rejected]: (state, action) => {
-      state.loading = false;
       state.error = action.payload;
     },
+    // getuser
   },
 });
 export default userSlice.reducer;
