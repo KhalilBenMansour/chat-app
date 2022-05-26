9d0f54988528bcdc8c5c741769e2b7ce3020128b
KhalilBenMansour <khalil.benmansour@enis.tn>
2022-05-26 11:22:04 +0100

    adding the route of verification using mail

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import { Routes, Route } from "react-router-dom";
 import Private from "./components/private/Private";
+import Welcome from "./components/welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Private />}>
-          <Route path="" element={<Container />} />
+          <Route path="/" element={<Container />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/verify/:id/:confirmCode" element={<Welcome />} />
       </Routes>
     </div>
   );
